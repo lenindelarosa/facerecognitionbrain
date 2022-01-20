dbc1e74c3b2717da4facca8afe17311876c03030
lenindelarosa <leninmanueld@gmail.com>
2022-01-20 10:49:18 -0400

    adding new project files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navigation />
+      <Logo />
+      <ImageLinkForm />
+      <FaceRecognition />
+    </div>
+  );
+}
+
+export default App;

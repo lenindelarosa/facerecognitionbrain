8ee666559a363aba5ea45c933906d02f12def0c1
lenindelarosa <leninmanueld@gmail.com>
2022-01-25 19:23:45 -0400

    adding 25/01 changes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,62 @@ import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import Particles from 'react-tsparticles';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <ImageLinkForm />
-     { /*
-      
-     <FaceRecognition />*/}
-    </div>
-  );
+
+const particlesOptions = {
+  "particles": {
+    "number": {
+        "value": 160,
+        "density": {
+            "enable": false
+        }
+    },
+    "size": {
+        "value": 10,
+        "random": true
+    },
+    "move": {
+        "direction": "bottom",
+        "out_mode": "out"
+    },
+    "line_linked": {
+        "enable": false
+    }
+},
+"interactivity": {
+    "events": {
+        "onclick": {
+            "enable": true,
+            "mode": "remove"
+        }
+    },
+    "modes": {
+        "remove": {
+            "particles_nb": 10
+        }
+    }
+}
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='' params={{particlesOptions}} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+      { /*
+        
+      <FaceRecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+        <div>
+            <div className="white f3 pa3">
+                {
+                    "Andrei, your current rank is..."
+                }
+            </div>
+            <div className="white f2">
+                {
+                    "#5"
+                }
+            </div>
+
+        </div>
+    );
+}
+
+export default Rank

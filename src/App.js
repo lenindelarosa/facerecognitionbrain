a58c2ff8586454b33c7c16a6e676fcdade59945e
lenindelarosa <leninmanueld@gmail.com>
2022-01-28 16:29:42 -0400

    adding latest functionality

--- src/App.js
+++ src/App.js
@@ -25,8 +25,29 @@ class App extends Component {
 		super();
 		this.state = {
 			input: '',
-			imageUrl: ''
+			imageUrl: '',
+			box: {},
 		}
+	}
+
+	calculateFaceLocation = (data) => {
+		const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+		const image = document.getElementById('inputimage')
+		const width = Number(image.width);
+		const height = Number(image.height);
+		console.log(width, height);
+		return {
+			leftCol: clarifaiFace.left_col * width,
+			topRow: clarifaiFace.top_row * height,
+			rightCol: width - (clarifaiFace.right_col * width),
+			bottomRow: height - (clarifaiFace.bottom_row * height)
+
+		}
+	}
+
+	displayFaceBox = (box) => {
+		console.log(box);
+		this.setState({box: box});
 	}
 
 	onInputChange = (event) => {
@@ -38,17 +59,12 @@ class App extends Component {
 		this.setState({imageUrl: this.state.input});
 		app.models
 			.predict(
-				Clarifai.COLOR_MODEL, 
+				Clarifai.FACE_DETECT_MODEL, 
 				this.state.input)
-			.then(
-				function (response) {
-					console.log(response)
-				},
-				function (err){
-					console.log(err)
-				}
-		)
+			.then(response => this.displayFaceBox(this.calculateFaceLocation(response)))
+			.catch(err => console.log(err))
 	}
+//console.log(response.outputs[0].data.regions[0].region_info.bounding_box)
 
   render() {
     return (
@@ -58,7 +74,7 @@ class App extends Component {
         <Logo />
         <Rank />
         <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
-		<FaceRecognition imageUrl={this.state.imageUrl}/>
+		<FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
       </div>
     );
   }
